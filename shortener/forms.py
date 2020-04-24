cb089cb771d552a56ae31299a60f88dfd38d692a
Vadim G <rrretry@ya.ru>
2020-04-24 12:45:25 +0500

    fix index template style issues

--- shortener/forms.py
+++ shortener/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class URLForm(forms.Form):
     bootstrap_styles = {"class": "form-control text-center", "placeholder": "URL"}
-    url_form_widget = forms.widgets.TextInput(attrs=bootstrap_styles)
+    url_form_widget = forms.widgets.URLInput(attrs=bootstrap_styles)
 
     url = forms.URLField(label='', widget=url_form_widget)
